2d3801e1779dad8e59da58c4dad9561dc58e02c8
Son Pham <son.pham@masterflex.com>
2025-02-24 08:14:23 -0600

    Reduce string size due to buffer overflow

--- samples/pn_dev/app_gsdml.c
+++ samples/pn_dev/app_gsdml.c
@@ -148,7 +148,7 @@ static const app_gsdml_submodule_t submod_motor_direction = {
    .api = APP_GSDML_API,
    .data_dir = PNET_DIR_IO,
    .insize = 0,
-   .outsize = 10,
+   .outsize = 3,
    .parameters = {0}};
 
 static const app_gsdml_submodule_t submod_echo = {
